6952b04c59f7fa87f4072da6f56f60043d1e961e
luoboQuQ <luobo110@163.com>
2021-03-10 16:26:24 +0800

    增加企业微信推送

--- PyAipao.py
+++ PyAipao.py
@@ -2,6 +2,7 @@ import json
 from random import randint, uniform
 import os
 
+from pushman import Servers
 import requests
 
 
@@ -98,8 +99,8 @@ class Aipaoer(object):
         try:
             if rsp.json()["Success"]:
                 fin = {'msg': 'success',
-                       'end': '|'+self.userName+'|'+str(my_speed)+'|'+str(my_distance) +
-                       '|'+str(my_costTime)+'|'+str(my_step)+'|'
+                       'end': "速度："+str(my_speed)+" 路程："+str(my_distance) +
+                       " 用时："+str(my_costTime)+" 时速："+str(my_step)
                        }
                 return fin
         except KeyError:
@@ -126,8 +127,6 @@ def weixin_send(SCKEY, body):
 
 
 def main():
-    ends = ["|姓名|&nbsp;速度&nbsp;|&nbsp;距离&nbsp;|&nbsp;时间&nbsp;|&nbsp;步数&nbsp;|",
-            "|:----:|:----:|:----:|:----:|:----:|"]
     try:
         IMEICode = os.environ['IMEICODE']
     except KeyError:
@@ -139,20 +138,22 @@ def main():
         aipaoer.get_runId()
         fin = aipaoer.upload_record()
         if(fin.get('msg') == 'success'):
-            ends.append(fin.get('end'))
+            ends = fin.get('end')
             text = "跑步结果-成功"
         else:
-            ends.append('|'+names[index]+'|error||||')
+            ends = "失败"
             text = "跑步结果-失败"
     else:
-        ends = ["IMEICode失效"]
+        ends = "IMEICode失效"
         text = "跑步结果-失败"
-    desp = "\r\n".join(ends)  # 将跑步详细结果转为字符串
-    print(desp)
-
+    print(ends)
+    servers = Servers("ww54f76dc199a97d51",
+                     "3MKLZX5D5yhSTeDUT_30yh9Tum16JMB8WSEAMHgLkGo", 1000003)
+    servers.get_access_token()
+    servers.send(text+'\n'+ends+'\n')
     body = {
         "text": text,
-        "desp": desp
+        "desp": ends
     }
     try:
         SCKEY = os.environ['SCKEY']
---
+++ pushman.py
@@ -0,0 +1,40 @@
+import json
+
+import requests
+
+
+class Servers(object):
+    def __init__(self, corpid, corpsecret, agentid):
+        self.access_token = ""
+        self.corpid = corpid
+        self.corpsecret = corpsecret
+        self.agentid = agentid
+
+    def get_access_token(self):
+        url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={ID}&corpsecret={SECRET}".format(
+            ID=self.corpid, SECRET=self.corpsecret)
+        rsp = requests.get(url)
+        try:
+            if rsp.json()["errcode"] == 0:
+                okJson = rsp.json()
+                self.access_token = okJson["access_token"]
+        except KeyError:
+            print("获取access_token失败")
+
+    def send_text(self, text):
+        json = {
+            "touser": "@all",
+            "msgtype": "text",
+            "agentid": self.agentid,
+            "text": {
+                "content": text
+            }
+        }
+        url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={ACCESS_TOKEN}".format(
+            ACCESS_TOKEN=self.access_token)
+        rsp = requests.post(url, json=json)
+        try:
+            if rsp.json()["errcode"] == 0:
+                return true
+        except:
+            return false
